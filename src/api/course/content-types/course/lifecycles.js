8258fffb57cc61a89ead19a72131bd30f0704dcc
enard <enarddpascua@gmail.com>
2022-06-20 20:57:35 +1000

    adding custom routes and controllers

--- src/api/course/content-types/course/lifecycles.js
+++ src/api/course/content-types/course/lifecycles.js
@@ -6,14 +6,12 @@ module.exports = {
     const {data} = event.params;
     if (data.course_name) {
       data.slug = slugify(data.course_name, { lower: true });
-      console.log(data)
     }
   },
   async beforeUpdate(event) {
     const {data} = event.params;
     if (data.course_name) {
      data.slug = slugify(data.course_name, { lower: true });
-     console.log("beforeUpdate", data)
     }
   },
 };
--- src/api/course/controllers/course.js
+++ src/api/course/controllers/course.js
@@ -6,4 +6,78 @@
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::course.course');
+module.exports = createCoreController('api::course.course', ({strapi}) => ({
+    //Create event with linked user
+    async create(ctx) {
+        console.log(ctx)
+        let entity;
+        if (ctx.is('multipart')) {
+          const { data, files } = parseMultipartData(ctx);
+          data.user = ctx.state.user.id;      
+          entity = await strapi.services.courses.create(data, { files });
+        } else {
+          ctx.request.body.user = ctx.state.user.id;
+          entity = await strapi.services.courses.create(ctx.request.body);
+        }
+        return sanitizeEntity(entity, { model: strapi.models.courses });
+      },
+
+      //Update user event
+      async update(ctx) {
+        const { id } = ctx.params;
+    
+        let entity;
+    
+        const [courses] = await strapi.services.courses.find({
+          id: ctx.params.id,
+          'user.id': ctx.state.user.id,
+        });
+    
+        if (!courses) {
+          return ctx.unauthorized(`You can't update this entry`);
+        }
+    
+        if (ctx.is('multipart')) {
+          const { data, files } = parseMultipartData(ctx);
+          entity = await strapi.services.courses.update({ id }, data, {
+            files,
+          });
+        } else {
+          entity = await strapi.services.courses.update({ id }, ctx.request.body);
+        }
+    
+        return sanitizeEntity(entity, { model: strapi.models.courses });
+      },
+
+      //Delete a user event
+      async delete(ctx){
+        const {id} = ctx.params;
+        const[courses] = await strapi.services.courses.find({
+            id: ctx.params.id,
+            "user.id": ctx.state.user.id,
+        });
+        if(!courses){
+            return ctx.unauthorized(`You can't update this entry`);
+        }
+        const entity = await strapi.services.courses.delete({id});
+        return sanitizeEntity(entity, {model: strapi.model.courses})
+      },
+
+    async me(ctx) {
+        const user = ctx.state.user.id
+        if(!user){
+            return ctx.badRequst(null, [
+                {messages: {id : "No authorization header was found"}}
+            ])
+        }
+
+        const data = await strapi.service['course'].find({user})
+
+        if(!data){
+            return ctx.notFound()
+        }
+
+        return sanitizeEntity(data, {model: strapi.models.courses})
+    }
+})
+);
